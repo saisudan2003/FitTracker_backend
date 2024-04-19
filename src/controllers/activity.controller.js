867757551670e85c221b9eae32692e60c43794b9
Sai Sudan SV <saisudan6@gmail.com>
2024-04-19 12:18:21 +0530

    getactivty update

--- src/controllers/activity.controller.js
+++ src/controllers/activity.controller.js
@@ -30,4 +30,26 @@ const addActivity = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, existingUserActivity, "Activity details updated successfully"));
 });
 
-export { addActivity };
+const getActivity = asyncHandler(async (req, res) => {
+    const today = new Date().toISOString().split('T')[0];
+    const username = req.user?._id;
+    let existingUserActivity = await Activity.findOne({ username, date: today });
+    if (!existingUserActivity) {
+        existingUserActivity = new Activity({ username, date: today });
+        existingUserActivity.steps = 0;
+        existingUserActivity.sleep = 0;
+        existingUserActivity.calories = 0;
+        await existingUserActivity.save();
+    }
+    let userData = {
+        steps: existingUserActivity.steps,
+        sleep: existingUserActivity.sleep,
+        calories: existingUserActivity.calories
+    }
+    return res
+    .status(200)
+    .json(new ApiResponse(200, userData, "Activity details fetched successfully"));
+
+})
+
+export { addActivity, getActivity };
--- src/routes/activity.routes.js
+++ src/routes/activity.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addActivity } from "../controllers/activity.controller.js";
+import { addActivity, getActivity } from "../controllers/activity.controller.js";
 
 const router = Router()
 
 router.route("/addupdateactivity").post(verifyJWT, addActivity)
+router.route("/getactivity").get(verifyJWT, getActivity)
 
 export default router
